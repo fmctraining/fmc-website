bb8e173c06f57a9041f2f4b7e8f45f7a2ce533c1
jldec <jurgen@jldec.me>
2025-07-10 15:03:56 -0400

    tighten prose-li

--- src/app/contentTheme/ContentHtml.tsx
+++ src/app/contentTheme/ContentHtml.tsx
@@ -2,5 +2,10 @@ import { requestInfo as r } from 'rwsdk/worker'
 
 export function ContentHtml() {
   const pageData = r.ctx.pageContext?.pageData
-  return <div className="prose prose-lg prose-invert prose-headings:text-primary prose-h1:font-semibold prose-h1:italic prose-p:text-white prose-li:text-white max-w-none" dangerouslySetInnerHTML={{ __html: pageData?.html ?? '[empty page]' }} />
+  return (
+    <div
+      className="prose prose-lg prose-invert prose-headings:text-primary prose-h1:font-semibold prose-h1:italic prose-p:text-white prose-li:text-white prose-hr:border-primary prose-hr:my-2 max-w-none"
+      dangerouslySetInnerHTML={{ __html: pageData?.html ?? '[empty page]' }}
+    />
+  )
 }
