6d57531698dbe707c695d23d6872377d1df9ac95
jldec <jurgen@jldec.me>
2025-07-25 19:47:08 +0100

    more subtle

--- src/app/contentTheme/DirList.tsx
+++ src/app/contentTheme/DirList.tsx
@@ -6,7 +6,7 @@ export function DirList() {
     <ul className="max-w-3xl m-auto">
       {pageData.dir.map((d) => (
         <li key={d.path} className="triangle-yellow text-lg py-1 my-4">
-          <a href={d.path} className="hover:underline">
+          <a href={d.path} className="hover:underline active:underline decoration-2">
             {d.attrs?.name || d.attrs?.title || d.path}
           </a>
         </li>
