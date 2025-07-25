7b39e033304557fcbfe3461594804f4dd34da069
jldec <jurgen@jldec.me>
2025-07-25 11:51:34 +0100

    extract NewsletterForm avoids ios safari history hydration

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -9,6 +9,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://rsms.me/" />
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      <script defer src="https://js.hsforms.net/forms/v2.js"></script>
     </head>
     <body>
       <div id="root">{children}</div>
