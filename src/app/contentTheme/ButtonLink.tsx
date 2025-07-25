7afb8bf39f3a48aaf3565edaa8a332a114366509
jldec <jurgen@jldec.me>
2025-07-25 17:28:22 +0100

    animate links

--- src/app/contentTheme/ButtonLink.tsx
+++ src/app/contentTheme/ButtonLink.tsx
@@ -2,7 +2,7 @@ export function ButtonLink({ href, text }: { href: string; text: string }) {
   return (
     <a
       href={href}
-      className="inline-block hover:underline decoration-2 underline-offset-8 py-2 px-3 sm:px-6 rounded-full bg-primary border-4 border-white"
+      className="inline-block hover:underline decoration-2 underline-offset-8 py-2 px-3 sm:px-6 rounded-full bg-primary border-4 border-white transition-transform duration-75 active:scale-96"
     >
       {text}
     </a>
