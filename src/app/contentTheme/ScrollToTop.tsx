90e4fecc1eecff684a02d54a0aa5f71cbeb43393
jldec <jurgen@jldec.me>
2025-07-25 12:35:41 +0100

    detect popstate and prevent scroll to top

--- src/app/contentTheme/ScrollToTop.tsx
+++ src/app/contentTheme/ScrollToTop.tsx
@@ -1,19 +1,31 @@
-"use client";
+'use client'
 
-import { useLayoutEffect } from "react";
+import { useLayoutEffect } from 'react'
 
 // https://discord.com/channels/679514959968993311/679514959968993476/1396963062111797328
 export function ScrollToTop() {
   useLayoutEffect(() => {
-    const observer = new MutationObserver(() => window.scrollTo(0, 0));
-    const main = document.querySelector("main");
+    let popStateWasCalled = false
+
+    const observer = new MutationObserver(() => {
+      // avoid scrolling to top after popstate events
+      if (!popStateWasCalled) {
+        // TODO: remove console.log
+        console.log('scroll to top')
+        window.scrollTo(0, 0)
+      }
+      popStateWasCalled = false
+    })
+
+    const main = document.querySelector('main')
 
     if (main) {
-      observer.observe(main, { childList: true, subtree: true });
+      window.addEventListener('popstate', () => (popStateWasCalled = true))
+      observer.observe(main, { childList: true, subtree: true })
     }
 
-    return () => observer.disconnect();
-  }, []);
+    return () => observer.disconnect()
+  }, [])
 
-  return null;
+  return null
 }
