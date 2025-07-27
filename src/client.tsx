e3cb6fa8e95792376757f66dd2ca2e0c236f4a41
jldec <jurgen@jldec.me>
2025-07-27 17:44:51 +0100

    simplify

--- src/client.tsx
+++ src/client.tsx
@@ -1,8 +1,4 @@
 import { initClient, initClientNavigation } from 'rwsdk/client'
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (!document.getElementById('404')) {
-    initClient()
-    initClientNavigation()
-  }
-})
+initClient()
+initClientNavigation()
