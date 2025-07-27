62cb77ed511794b1c1c92f3a0f33167cc5f7f963
jldec <jurgen@jldec.me>
2025-07-27 15:22:06 +0100

    remove stylesheet entirely

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -1,11 +1,8 @@
-import styles from './styles.css?url'
-
 export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href={styles} />
       <link rel="icon" href="/favicon.ico" />
       <link rel="modulepreload" href="/src/client.tsx" />
     </head>
