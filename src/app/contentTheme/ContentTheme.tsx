cd55be9e5e77a10e49dd45711ba06c4f3923b0f8
jldec <jurgen@jldec.me>
2025-08-03 09:03:01 +0100

    link to rwsdk PR #633 pp-allow-status-codes

--- src/app/contentTheme/ContentTheme.tsx
+++ src/app/contentTheme/ContentTheme.tsx
@@ -1,5 +1,4 @@
-import { type RequestInfo, renderToStream } from 'rwsdk/worker'
-import { Document } from '@/app/Document'
+import { requestInfo } from 'rwsdk/worker'
 import { NotFound } from './404'
 import { Page } from './Page'
 import { Home } from './Home'
@@ -8,8 +7,8 @@ import { CourseGroup } from './CourseGroup'
 import { Vendor } from './Vendor'
 import { UpcomingClasses } from './UpcomingClasses'
 
-export async function ContentTheme({ ctx, request }: RequestInfo) {
-  const pageData = ctx.pageContext?.pageData
+export async function ContentTheme() {
+  const pageData = requestInfo.ctx.pageContext?.pageData
   const layout = pageData?.attrs?.layout
   if (pageData) {
     switch (layout) {
@@ -27,15 +26,7 @@ export async function ContentTheme({ ctx, request }: RequestInfo) {
         return <Page />
     }
   } else {
-    // TODO: replace with requestInfo.status = 404 when available
-    // https://github.com/redwoodjs/sdk/issues/568
-    console.log(`404: ${request.url}`)
-    const url = new URL(request.url)
-    if (url.searchParams.has('__rsc')) {
-      return <NotFound />
-    }
-    return new Response(await renderToStream(<NotFound />, { Document }), {
-      status: 404
-    })
+    requestInfo.response.status = 404
+    return <NotFound />
   }
 }
--- src/client.tsx
+++ src/client.tsx
@@ -1,6 +1,4 @@
 import { initClient, initClientNavigation } from 'rwsdk/client'
 
-if (!document.getElementById('404')) {
-  const { handleResponse } = initClientNavigation()
-  initClient({ handleResponse })
-}
+initClient()
+initClientNavigation()
