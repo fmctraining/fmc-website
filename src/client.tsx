450b4cfadc12e254d7c31eede5493c8c8752766c
jldec <jurgen@jldec.me>
2025-08-08 15:29:20 +0100

    no intersectionobserver on small screens

--- src/client.tsx
+++ src/client.tsx
@@ -3,52 +3,53 @@ import { initClient, initClientNavigation } from 'rwsdk/client'
 
 initClient()
 
-const Observer = {
-  observer: null,
-  delayBetweenAnimations: 100,
-  animationCounter: 0,
-  reducedMotion: window?.matchMedia('(prefers-reduced-motion: reduce)').matches,
+const reducedMotion = window?.matchMedia('(prefers-reduced-motion: reduce)').matches
+const smallScreen = !window?.matchMedia('(min-width: 768px)').matches
+if (!reducedMotion && !smallScreen) {
+  const Observer = {
+    observer: null,
+    delayBetweenAnimations: 100,
+    animationCounter: 0,
 
-  start() {
-    const elements = Array.from(document.querySelectorAll('.intersect-animate'))
-    elements.forEach((el) => {
-      el.setAttribute('data-wait-for-animation', '')
-    })
-    const callback = (entries) => {
-      entries.forEach((entry) => {
-        requestAnimationFrame(() => {
-          const target = entry.target
+    start() {
+      const elements = Array.from(document.querySelectorAll('.intersect-animate'))
+      elements.forEach((el) => {
+        el.setAttribute('data-wait-for-animation', '')
+      })
+      const callback = (entries) => {
+        entries.forEach((entry) => {
+          requestAnimationFrame(() => {
+            const target = entry.target
 
-          if (entry.intersectionRatio >= 0.25) {
-            if (!target.hasAttribute('data-animated')) {
-              target.removeAttribute('data-wait-for-animation')
-              target.setAttribute('data-animated', 'true')
+            if (entry.intersectionRatio >= 0.25) {
+              if (!target.hasAttribute('data-animated')) {
+                target.removeAttribute('data-wait-for-animation')
+                target.setAttribute('data-animated', 'true')
 
-              const delay = this.animationCounter++ * this.delayBetweenAnimations
-              target.style.animationDelay = `${delay}ms`
-              // only animate once
-              this.observer.unobserve(target)
+                const delay = this.animationCounter++ * this.delayBetweenAnimations
+                target.style.animationDelay = `${delay}ms`
+                // only animate once
+                this.observer.unobserve(target)
+              }
+            } else {
+              target.setAttribute('data-wait-for-animation', '')
+              target.removeAttribute('data-animated')
+              target.style.transitionDelay = ''
+              target.style.animationDelay = ''
+
+              this.animationCounter = 0
             }
-          } else {
-            target.setAttribute('data-wait-for-animation', '')
-            target.removeAttribute('data-animated')
-            target.style.transitionDelay = ''
-            target.style.animationDelay = ''
-
-            this.animationCounter = 0
-          }
+          })
         })
+      }
+
+      this.observer = new IntersectionObserver(callback.bind(this), { threshold: [0, 0.25, 0.5, 0.99] })
+
+      elements.forEach((el) => {
+        this.observer.observe(el)
       })
     }
-
-    this.observer = new IntersectionObserver(callback.bind(this), { threshold: [0, 0.25, 0.5, 0.99] })
-
-    elements.forEach((el) => {
-      this.observer.observe(el)
-    })
   }
-}
 
-if (!Observer.reducedMotion) {
   Observer.start()
 }
