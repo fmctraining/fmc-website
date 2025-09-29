363749a65c2ff2d6c56c941877ca79eb0b3e973e
jldec <jurgen@jldec.me>
2025-09-29 14:45:02 -0400

    render raw content

--- src/app/contentSource/contentMiddleware.tsx
+++ src/app/contentSource/contentMiddleware.tsx
@@ -12,11 +12,25 @@ export type contentMiddlewareOptions = {
   ignore?: string | string[]
 }
 
+function acceptRaw(accept: string | null) {
+  if (!accept) return false
+  accept = accept.toLowerCase()
+
+  const html = accept.indexOf('text/html')
+  const plain = accept.indexOf('text/plain')
+  const markdown = accept.indexOf('text/markdown')
+
+  if (plain === -1 && markdown === -1) return false
+  if (html === -1 || (html > plain && html > markdown)) return true
+  return false
+}
+
 export const contentMiddleware = ({ ignore }: contentMiddlewareOptions = {}) => {
   return async ({ request, ctx }: RequestInfo): Promise<Response | void> => {
     const noCacheRead =
       request.headers.get('cache-control')?.includes('no-cache') || request.headers.get('pragma')?.includes('no-cache')
     const url = new URL(request.url)
+    const prefersRaw = url.searchParams.has('raw') || acceptRaw(request.headers.get('accept'))
     const pathname = url.pathname
     const search = url.search
 
@@ -59,6 +73,39 @@ export const contentMiddleware = ({ ignore }: contentMiddlewareOptions = {}) => 
       courseData
     }
     if (url.searchParams.has('json')) return Response.json(pageContext)
+    if (prefersRaw) {
+      // imperfect raw rendering - does not include /terms /press etc.
+      let markdown = ''
+      if (pageContext.pageData?.attrs?.name || pageContext.pageData?.attrs?.title) {
+        markdown += '# ' + (pageContext.pageData?.attrs?.name || pageContext.pageData?.attrs?.title) + '\n\n'
+      }
+      if (pageContext.pageData?.md) {
+        markdown += pageContext.pageData.md
+      }
+      if (pageContext.pageData?.attrs?.classes?.length) {
+        markdown +=
+          '\n\n### classes\n' +
+          pageContext.pageData.attrs.classes.map((entry) => `- [${entry.name}](${entry.href})`).join('\n')
+      }
+      if (pageContext.pageData?.dir?.length) {
+        markdown +=
+          '\n\n### links\n' +
+          pageContext.pageData.dir.map((entry) => `- [${entry.attrs?.title || entry.path}](${entry.path})`).join('\n')
+      }
+      if (pageContext.pageData?.attrs?.footer?.links.length) {
+        markdown += '\n\n ### links\n' +
+        pageContext.pageData.attrs.footer.links.map((entry) => `- [${entry.text}](${entry.href})`).join('\n')
+      }
+      if (pageContext.pageData?.attrs?.footer?.social?.length) {
+        markdown += '\n\n ### social\n' +
+        pageContext.pageData.attrs.footer.social.map((entry) => `- [${entry.text}](${entry.href})`).join('\n')
+      }
+      if (pageContext.siteData?.footer?.terms?.length) {
+        markdown += '\n\n ### terms\n' +
+        pageContext.siteData.footer.terms.map((entry) => `- [${entry.text}](${entry.href})`).join('\n')
+      }
+      return markdown ? new Response(markdown) : new Response('not found', { status: 404 })
+    }
     ctx.pageContext = pageContext
   }
 }
